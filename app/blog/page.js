cb4d3db20fa61e8424b9e95e9ab9d640b5755e4d
Daisuke MATSUMOTO <dmatsu2006@gmail.com>
2023-07-06 14:26:11 +0900

    プログラム修正

--- app/blog/page.js
+++ app/blog/page.js
@@ -7,36 +7,54 @@ import matter from "gray-matter"
 
 async function getAllBlogs() {
 	const files = fs.readdirSync(path.join("data"))
+	// console.log(files)
 	const blogs = files.map((fileName) => {
 		const slug = fileName.replace(".md", "")
 		const fileData = fs.readFileSync(
 			path.join("data", fileName),
-			"utf8"
+			"utf-8"
 		)
+		// console.log(fileData)
 		const { data } = matter(fileData)
+		// console.log(data)
 		return {
 			frontmatter: data,
 			slug: slug,
 		}
 	})
+	const orderedBlogs = blogs.sort((a, b) => {
+		return b.frontmatter.id - a.frontmatter.id
+	})
 	return {
-		blogs: blogs
+		blogs: orderedBlogs
 	}
 }
 
 const Blog = async() => {
 	const { blogs } = await getAllBlogs()
+	console.log(blogs)
 	return (
+		<>
 		<div>
-			<h1>ブログページ</h1>
-			{blogs.map((blog, index) =>
-				<div key={index}>
-					<h2>{blog.frontmatter.title}</h2>
-					<p>{blog.frontmatter.date}</p>
-					<Link href={`/blog/${blog.slug}`}>Read More</Link>
-				</div>
-			)}
+			<div>
+				<h1>Blog</h1>
+				<p>エンジニアの日常生活をお届けします</p>
+					{blogs.map((blog, index) =>
+						<div key={index}>
+							<div>
+								<h2>{blog.frontmatter.title}</h2>
+								<p>{blog.frontmatter.excerpt}</p>
+								<p>{blog.frontmatter.date}</p>
+								<Link href={`/blog/${blog.slug}`}>Read More</Link>
+							</div>
+							<div>
+
+							</div>
+						</div>
+					)}
+			</div>
 		</div>
+		</>
 	)
 }
 
